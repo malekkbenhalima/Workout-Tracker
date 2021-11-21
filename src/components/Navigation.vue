8bf3bc42fe5f89052beaed6618a82acfdde59d00
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-22 00:28:28 +0100

    Logo behaves as home route

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,10 +2,12 @@
   <header>
     <header class="bg-at-light-green text-white">
       <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
-        <div class="flex items-center gap-x-4">
-          <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
-          <h1 class="text-lg">Workout Tracker</h1>
-        </div>
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }">
+          <div class="flex items-center gap-x-4">
+            <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
+            <h1 class="text-lg">Workout Tracker</h1>
+          </div>
+        </router-link>
         <ul class="flex flex-1 justify-end gap-x-10">
           <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
           <router-link class="cursor-pointer" :to="{ name: 'Login' }">Create</router-link>
