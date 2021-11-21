f818533ca0e3ec4ff1258252207dd35b2bc61a24
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-21 20:09:13 +0100

    Add Navigation component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,20 @@
 <template>
-  <header></header>
+  <header>
+    <header class="bg-at-light-green text-white">
+      <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+        <div class="flex items-center gap-x-4">
+          <img class="w-14" src="../assets/images/dumbbell-light.png" alt="">
+          <h1 class="text-lg">Workout Tracker</h1>
+        </div>
+        <ul class="flex flex-1 justify-end gap-x-10">
+          <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+          <router-link class="cursor-pointer" :to="{ name: 'Home' }">Create</router-link>
+          <router-link class="cursor-pointer" :to="{ name: 'Home' }">Login</router-link>
+          <li class="cursor-pointer">Logout</li>
+        </ul>
+      </nav>
+    </header>
+  </header>
 </template>
 
 <script>
