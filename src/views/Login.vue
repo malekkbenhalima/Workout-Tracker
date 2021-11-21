f6ccfd300c81dfbfb2c3f2278bcd4ad280be32b0
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-22 00:13:37 +0100

    Fix form button type

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -32,7 +32,7 @@
         />
       </div>
 
-      <button type="button" class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+      <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm
       text-white bg-at-light-green duration-200 border-solid
       border-2 border-transparent hover:border-at-light-green hover:bg-white
       hover:text-at-light-green">Login
