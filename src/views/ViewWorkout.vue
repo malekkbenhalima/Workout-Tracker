2ad4a9aaa72b28d010f8354bc0e58f4256676fa0
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-28 15:13:34 +0100

    Add the ability to edit a workout

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -9,6 +9,37 @@
         {{ errorMsg }}
       </p>
     </div>
+
+    <div v-if="dataLoaded">
+      <!-- General Workout Info -->
+      <div class="flex flex-col items-center p-8 rounded-md shadow-md bg-light-grey relative">
+
+        <div v-if="user" class="flex absolute left-2 top-2 gap-x-2">
+          <div @click="editMode" class="h-7 w-7 rounded-full flex justify-center items-center cursor-pointer bg-at-light-green shadow-lg">
+            <img class="h-3.5 w-auto" src="../assets/images/pencil-light.png" alt="">
+          </div>
+
+          <div class="h-7 w-7 rounded-full flex justify-center items-center cursor-pointer bg-at-light-green shadow-lg">
+            <img class="h-3.5 w-auto" src="../assets/images/trash-light.png" alt="">
+          </div>
+        </div>
+
+        <img v-if="data.workoutType === 'cardio'" class="h-24 w-auto" src="../assets/images/running-light-green.png" alt="">
+
+        <img v-else class="h-24 w-auto" src="../assets/images/dumbbell-light-green.png" alt="">
+
+        <span class="mt-6 py-1.5 px-4 text-xs text-white bg-at-light-green rounded-lg shadow-md">
+          {{ data.workoutType }}
+        </span>
+
+        <div class="w-full mt-6">
+          <input v-if="edit" type="text" class="w-2 w-full text-gray-500 focus:outline-none" v-model="data.workoutName">
+          <h1 v-else class="text-at-light-green text-2xl text-center">
+            {{ data.workoutName }}
+          </h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,7 +68,7 @@ export default {
       try {
         const {data: workouts, error} = await supabase.from('workouts').select('*').eq('id', currentId);
         if (error) throw error;
-        data.value = workouts;
+        data.value = workouts[0];
         dataLoaded.value = true;
         console.log(data.value)
       } catch (error) {
@@ -53,6 +84,11 @@ export default {
     // Delete workout
 
     // Edit mode
+    const edit = ref(null);
+
+    const editMode = () => {
+      edit.value = !edit.value;
+    }
 
     // Add exercise
 
