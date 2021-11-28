c494625fa6d41fbde694e61412479bf0a2e80035
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-28 14:59:48 +0100

    Get workout data and prepare template markup

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -1,16 +1,54 @@
 <template>
-  <div></div>
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <!-- App Msg -->
+    <div v-if="statusMsg || errorMsg" class="mb-10 p-4 rounded-md shadow-md bg-at-light-green">
+      <p class="text-at-light-green">
+        {{ statusMsg }}
+      </p>
+      <p class="text-red-500">
+        {{ errorMsg }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
+import {ref, computed} from "vue"
+import {supabase} from "../supabase/init";
+import {useRoute} from 'vue-router'
+import store from '../store/index'
+
 export default {
   name: "view-workout",
   setup() {
     // Create data / vars
+    const data = ref(null)
+    const dataLoaded = ref(null)
+    const errorMsg = ref(null)
+    const statusMsg = ref(null)
+    const route = useRoute()
+    const user = computed(() => store.state.user);
 
     // Get current Id of route
+    const currentId = route.params.workoutId;
 
     // Get workout data
+    const getData = async () => {
+      try {
+        const {data: workouts, error} = await supabase.from('workouts').select('*').eq('id', currentId);
+        if (error) throw error;
+        data.value = workouts;
+        dataLoaded.value = true;
+        console.log(data.value)
+      } catch (error) {
+        errorMsg.value = error.message;
+        setTimeout(() => {
+          errorMsg.value = false
+        }, 5000)
+      }
+    }
+
+    getData();
 
     // Delete workout
 
@@ -22,7 +60,7 @@ export default {
 
     // Update Workout
 
-    return {};
+    return {statusMsg, data, dataLoaded, errorMsg, edit, editMode, user};
   },
 };
 </script>
