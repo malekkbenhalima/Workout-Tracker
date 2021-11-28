9bc47f2312fa4a658edc390e295c6fda25a0f020
malekkbenhalima <malekbenhlima@icloud.com>
2021-11-28 01:18:03 +0100

    Add template markup

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,12 +1,137 @@
 <template>
-  <div></div>
-</template>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <!-- Status Message -->
+    <div v-if="statusMsg || errorMsg" class="mb-10 p-4 bg-light-grey rounded-md shadow-lg">
+      <p class="text-at-light-green">
+        {{ statusMsg }}
+      </p>
+      <p class="text-red-500">
+        {{ errorMsg }}
+      </p>
+    </div>
 
+    <!-- Create -->
+    <div class="p-8 flex items-start bg-light-grey rounded shadow-lg">
+      <!-- Form -->
+      <form action="" class="flex flex-col gap-y-5 w-full">
+
+        <h1 class="text-2xl text-at-light-green">Record Workout</h1>
+
+        <!-- Workout Name -->
+        <div class="flex flex-col">
+          <label for="workout-name" class="mb-1 text-sm text-at-light-green">Workout Name</label>
+          <input type="text" required class="p-2 text-gray-500 focus:outline-none" id="workout-name"
+                 v-model="workoutName">
+        </div>
+
+        <!-- Workout Type -->
+        <div class="flex flex-col">
+          <label for="workout-type" class="mb-1 text-sm text-at-light-green">Workout Type</label>
+          <select id="workout-type" class="p-2 text-gray-500 focus:outline-none" required v-model="workoutType">
+            <option value="select-workout">Select Workout</option>
+            <option value="strength">Strength Training</option>
+            <option value="cardio">Cardio</option>
+          </select>
+        </div>
+
+        <!-- Strength Training Input -->
+        <div v-if="workoutType === 'strength'" class="flex flex-col gap-y-4">
+          <div class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row" v-for="(item, index) in exercises"
+               :key="index">
+            <div class="flex flex-col md:w-1/3">
+              <label for="exercise-name" class="mb-1 text-sm text-at-light-green">Exercise</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="exercise-name"
+                     v-model="item.exercise">
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="sets" class="mb-1 text-sm text-at-light-green">Sets</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="sets"
+                     v-model="item.sets">
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="reps" class="mb-1 text-sm text-at-light-green">Reps</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="reps"
+                     v-model="item.reps">
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="weight" class="mb-1 text-sm text-at-light-green">Weight (KG)</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="weight"
+                     v-model="item.weight">
+            </div>
+
+            <img src="@/assets/images/trash-light-green.png" class="h-4 w-auto absolute -left-5 cursor-pointer" alt="">
+          </div>
+          <button type="button" class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+            text-white bg-at-light-green duration-200 border-solid
+            border-2 border-transparent hover:border-at-light-green hover:bg-white
+            hover:text-at-light-green">Add Exercise
+          </button>
+        </div>
+
+        <!-- Cardio Input -->
+        <div v-if="workoutType === 'cardio'" class="flex flex-col gap-y-4">
+          <div class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row" v-for="(item, index) in exercises"
+               :key="index">
+            <div class="flex flex-col md:w-1/3">
+              <label for="cardio-type" class="mb-1 text-sm text-at-light-green">Type</label>
+              <select id="cardio-type" class="p-2 w-full text-gray-500 focus:outline-none" v-model="item.cardioType">
+                <option value="#">Select Type</option>
+                <option value="run">Runs</option>
+                <option value="walk">Walk</option>
+              </select>
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="distance" class="mb-1 text-sm text-at-light-green">Distance</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="distance"
+                     v-model="item.sets">
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="duration" class="mb-1 text-sm text-at-light-green">Duration</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="duration"
+                     v-model="item.duration">
+            </div>
+
+            <div class="flex flex-col md:flex-1">
+              <label for="pace" class="mb-1 text-sm text-at-light-green">Pace</label>
+              <input required type="text" class="p-2 w-full text-gray-500 focus:outline-none" id="pace"
+                     v-model="item.pace">
+            </div>
+
+            <img src="@/assets/images/trash-light-green.png" class="h-4 w-auto absolute -left-5 cursor-pointer" alt="">
+          </div>
+          <button type="button" class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+            text-white bg-at-light-green duration-200 border-solid
+            border-2 border-transparent hover:border-at-light-green hover:bg-white
+            hover:text-at-light-green">Add Exercise
+          </button>
+        </div>
+
+        <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+            text-white bg-at-light-green duration-200 border-solid
+            border-2 border-transparent hover:border-at-light-green hover:bg-white
+            hover:text-at-light-green">Record Workout
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
 <script>
+import {ref} from "vue";
+
 export default {
   name: "create",
   setup() {
     // Create data
+    const workoutName = ref("");
+    const workoutType = ref("select-workout")
+    const exercises = ref([1]);
+    const statusMsg = ref(null);
+    const errorMsg = ref(null);
 
     // Add exercise
 
@@ -16,7 +141,7 @@ export default {
 
     // Create workout
 
-    return {};
+    return {workoutName, workoutType, exercises, statusMsg, errorMsg};
   },
 };
 </script>
